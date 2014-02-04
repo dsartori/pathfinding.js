2193f8b7239a2fd4fe5362eaa6f682a78e6bd3db
dsartori <doug@sartori.ca>
2014-02-04 11:34:57 -0500

    Taar-ize the method prototypes.  Add descriptive comments.

--- pathfinding.js
+++ pathfinding.js
@@ -1,80 +1,91 @@
+// use Grid.prototype.show = function(){ }
+
+/*
+print = function(){
+    if (print !=null){
+        return print;
+    }else{
+        return console.log;
+    }
+*/
+
 function Grid(){
 
 
-this.matrix = [	['0','0','1','0','0','1','0','0','0','0','0','0','0','0'],
-				['0','0','1','0','0','0','0','0','1','1','1','1','1','1'],
-				['0','0','0','0','1','0','1','0','0','0','0','0','0','0'],
-				['1','1','1','0','1','0','1','0','1','1','1','1','1','0'],
-				['0','0','0','0','1','0','1','0','0','0','0','0','1','0'],
-				['0','0','0','0','1','0','1','1','1','1','0','0','1','1'],
-				['0','1','1','0','1','1','1','0','0','0','1','0','1','0'],
-				['0','0','0','0','1','0','0','0','0','0','1','0','1','0'],
-				['0','0','0','0','1','0','1','0','1','0','1','0','0','0'],	
-				['0','0','0','0','0','0','0','0','1','0','1','1','1','0'],
-				['0','0','0','0','0','0','0','0','1','0','0','0','1','0'],
-				['0','0','0','0','0','0','0','0','1','0','0','0','0','0'],
-				['0','0','0','0','0','0','0','0','1','0','0','0','0','0'],
-				['0','0','0','0','0','0','0','0','1','0','0','0','0','0']];
+this.matrix = [ ['0','0','1','0','0','1','0','0','0','0','0','0','0','0'],
+                ['0','0','1','0','0','0','0','0','1','1','1','1','1','1'],
+                ['0','0','0','0','1','0','1','0','0','0','0','0','0','0'],
+                ['1','1','1','0','1','0','1','0','1','1','1','1','1','0'],
+                ['0','0','0','0','1','0','1','0','0','0','0','0','1','0'],
+                ['0','0','0','0','1','0','1','1','1','1','0','0','1','1'],
+                ['0','1','1','0','1','1','1','0','0','0','1','0','1','0'],
+                ['0','0','0','0','1','0','0','0','0','0','1','0','1','0'],
+                ['0','0','0','0','1','0','1','0','1','0','1','0','0','0'],  
+                ['0','0','0','0','0','0','0','0','1','0','1','1','1','0'],
+                ['0','0','0','0','0','0','0','0','1','0','0','0','1','0'],
+                ['0','0','0','0','0','0','0','0','1','0','0','0','0','0'],
+                ['0','0','0','0','0','0','0','0','1','0','0','0','0','0'],
+                ['0','0','0','0','0','0','0','0','1','0','0','0','0','0']];
 
     this.xSize = this.matrix[0].length;
     this.ySize = this.matrix.length;
 
     // visited matrix marks visited nodes
-	this.visited = copy(this.matrix);
+    this.visited = copy(this.matrix);
 
 
-	// print the matrix
-	this.show = function(){
+    // print the matrix
+    Grid.prototype.show = function(){
 
-		for (var i = 0; i < this.visited.length; i++){
-			print('[' + this.visited[i] + ']');
-		}
-	}
+        for (var i = 0; i < this.visited.length; i++){
+            print('[' + this.visited[i] + ']');
+        }
+    }
 
-	// return a list of all adjacent, unvisited nodes
-	this.possibleMoves = function(p){
+    // return a list of all adjacent, unvisited nodes
+    Grid.prototype.possibleMoves = function(p){
 
-		x = p[0];
-		y = p[1];
-		var moves = [];
-		for (var i=-1; i < 2; i++){
-			for (var j=-1; j < 2; j++){
-				
-				xAdj = x+j;
-				yAdj = y+i;
+        x = p[0];
+        y = p[1];
+        var moves = [];
+        for (var i=-1; i < 2; i++){
+            for (var j=-1; j < 2; j++){
+                
+                xAdj = x+j;
+                yAdj = y+i;
 
-				if ((i !=0 || j!=0) 
-					&& ((xAdj > -1 && xAdj < this.xSize))
-					&& ((yAdj < this.ySize && yAdj > -1))){ 
+                if ((i !=0 || j!=0) 
+                    && ((xAdj > -1 && xAdj < this.xSize))
+                    && ((yAdj < this.ySize && yAdj > -1))){ 
 
-					if (this.visited[yAdj][xAdj] == 0){
-						var m = new Array()
-						m[0] = xAdj;
-						m[1] = yAdj;
-						moves.push(m);
-					}
-				}
-			}
-		}
-		return moves;
-	}
+                    if (this.visited[yAdj][xAdj] == 0){
+                        var m = new Array()
+                        m[0] = xAdj;
+                        m[1] = yAdj;
+                        moves.push(m);
+                    }
+                }
+            }
+        }
+        return moves;
+    }
 
-	// has this node been visited?
-	this.checkVisited = function(position){
+    // has this node been visited?
+    Grid.prototype.checkVisited = function(position){
 
-		x = position[0];
-		y = position[1];
+        x = position[0];
+        y = position[1];
 
-		return (this.visited[y][x]);
-	}
+        return (this.visited[y][x]);
+    }
 
-	this.clearVisited = function(){
-		this.visited = this.matrix;
-	}
+    Grid.prototype.clearVisited = function(){
+        this.visited = this.matrix;
+    }
 
-	this.visit = function(position){
-		this.visited[position[1]][position[0]] = '2';
-	}
+    Grid.prototype.visit = function(position){
+        this.visited[position[1]][position[0]] = '2';
+    }
 }
 
 
@@ -83,288 +94,318 @@ this.matrix = [	['0','0','1','0','0','1','0','0','0','0','0','0','0','0'],
 // pretty-print an array of moves
 function showMoves(arr){
 var str = arr.length + ' moves:';
-	for (var i = 0; i < arr.length ; i++){
-		if (i > 0){ 
-			str += ',';
-		}
-		str += '{' + arr[i] + '}';
-	}
+    for (var i = 0; i < arr.length ; i++){
+        if (i > 0){ 
+            str += ',';
+        }
+        str += '{' + arr[i] + '}';
+    }
 print(str);
 }
 
 // array copy
 function copy(arr){
-	var newArr = arr.slice(0);
-	for(var i = newArr.length; i--;)
-		if(newArr[i] instanceof Array)
-			newArr[i] = copy(newArr[i]);
-	return newArr;
+    var newArr = arr.slice(0);
+    for(var i = newArr.length; i--;)
+        if(newArr[i] instanceof Array)
+            newArr[i] = copy(newArr[i]);
+    return newArr;
 }
 
 // object clone XXX
 function clone(obj){
-	if(obj == null || typeof(obj) != 'object')
-		return obj;
+    if(obj == null || typeof(obj) != 'object')
+        return obj;
 
-	var temp = new obj.constructor(); 
-	for(var key in obj)
-		temp[key] = clone(obj[key]);
+    var temp = new obj.constructor(); 
+    for(var key in obj)
+        temp[key] = clone(obj[key]);
 
-	return temp;
+    return temp;
 }
 
 // check a move queue for equivalent move
 function contains(queue,position){
 
-	if (queue.length ==0)
-		return 0;
+    if (queue.length ==0)
+        return 0;
 
-	for (var i = 0; i < queue.length; i++){
-		var item = queue[i];	
-		if (item[0] == position[0] && item[1] == position[1]){
-			return 1;
-		}
+    for (var i = 0; i < queue.length; i++){
+        var item = queue[i];    
+        if (item[0] == position[0] && item[1] == position[1]){
+            return 1;
+        }
 
-	}
-	return 0;
+    }
+    return 0;
 }
 
 // construct key for hashes
 function makeKey(position){
 
-	var x = position[0];
-	var y = position[1];
- 	return x+","+y;
+    var x = position[0];
+    var y = position[1];
+    return x+","+y;
 
 }
 
 // search functions
 function Search(){
 
-	this.predecessor = {};
-	this.debug = 0;
-	this.count = 0;
+    this.predecessor = {};
+    this.debug = 0;
+    this.count = 0;
 
 
-	this.depthFirstSearch = function(matrix,position,goal){
+    Search.prototype.depthFirstSearch = function(matrix,position,goal){
 
-		this.count++;
-		matrix.visit(position);
+        this.count++;
 
-		if (position[0] == goal[0] && position[1]==goal[1]){
-			return this.pathFrom(goal);
-		}
+        // Mark the current node as visited
+        matrix.visit(position);
 
-		var moves = matrix.possibleMoves(position);
+        // Check if goal reached
+        if (position[0] == goal[0] && position[1] == goal[1]){
+            return this.pathFrom(goal);
+        }
 
-		if (this.debug)
-			matrix.show();
+        // Get all possible moves from current node
+        var moves = matrix.possibleMoves(position);
 
-		for (var i = 0; i < moves.length ; i++){
+        if (this.debug)
+            matrix.show();
 
-					if (this.debug)
-						print ("move = " + moves[i]);
+        for (var i = 0; i < moves.length ; i++){
 
-					this.predecessor[makeKey(moves[i])] = position;
-					newMatrix = clone(matrix);
-					if (this.depthFirstSearch(newMatrix,moves[i],goal)){
-						return this.pathFrom(goal);
-					}
+                    if (this.debug)
+                        print ("move = " + moves[i]);
 
-		}
+                    // Record breadcrumb path
+                    this.predecessor[makeKey(moves[i])] = position;
 
-	}
- 	this.breadthFirstSearch = function(matrix,position,goal){
+                    // Clone grid and recursively call DFS
+                    newMatrix = clone(matrix);
+                    if (this.depthFirstSearch(newMatrix,moves[i],goal)){
+                        return this.pathFrom(goal);
+                    }
 
- 		
- 		var queue = [[]];
- 		queue[0]=position;
+        }
 
- 		while (queue.length > 0){
- 			this.count++;
- 			var currentPosition = queue[0];
- 			queue.shift();
+    }
+    Search.prototype.breadthFirstSearch = function(matrix,position,goal){
 
-					if (this.debug){
-						print ("move = " + currentPosition);
-	
-						print ("predecessor = " + this.predecessor[makeKey(currentPosition)]);
-					}
-			
- 			if (currentPosition[0] == goal[0] && currentPosition[1] == goal[1]){
- 				return this.pathFrom(goal);
- 			}
- 			matrix.visit(currentPosition);
+        // Set up queue 
+        var queue = [[]];
+        queue[0]=position;
 
- 			if (this.debug)
- 				matrix.show();
+        while (queue.length > 0){
+            this.count++;
 
- 			var moves = matrix.possibleMoves(currentPosition);
- 			for (i = 0; i < moves.length; i++){
- 				if (!contains(queue,moves[i])){
+            // Take first element from queue for evaluation, then remove it
+            var currentPosition = queue[0];
+            queue.shift();
 
-					this.predecessor[makeKey(moves[i])] = currentPosition;
-					queue.push(moves[i]);
-				}
-			}
- 		}
+                    if (this.debug){
+                        print ("move = " + currentPosition);
+    
+                        print ("predecessor = " + this.predecessor[makeKey(currentPosition)]);
+                    }
+            // Check if goal is reached
+            if (currentPosition[0] == goal[0] && currentPosition[1] == goal[1]){
+                return this.pathFrom(goal);
+            }
+            matrix.visit(currentPosition);
 
-	}
+            if (this.debug)
+                matrix.show();
 
- 	this.aStar = function(matrix,position,goal){
+            // Get all possible moves from current node 
+            var moves = matrix.possibleMoves(currentPosition);
+            for (i = 0; i < moves.length; i++){
+                if (!contains(queue,moves[i])){
 
- 		var that = this;
- 		function calculateScore(move,g){
+                    // Add new moves found to the front of the queue
+                    this.predecessor[makeKey(moves[i])] = currentPosition;
+                    queue.push(moves[i]);
+                }
+            }
+        }
 
- 			var goalX = g[0];
- 			var goalY = g[1];
+    }
 
- 			var x = move[0];
-			var y = move[1];
+    Search.prototype.aStar = function(matrix,position,goal){
 
-			// cost to get here
- 			g = depth[makeKey(move)];
+        // Function to calculate a node's score
+        function calculateScore(move,g){
 
- 			// Manhattan heuristic
- 			deltaX = Math.abs(x - goalX);
- 			deltaY = Math.abs(y - goalY);
+            // Goal location 
+            var goalX = g[0];
+            var goalY = g[1];
 
- 			var h =  Math.sqrt(( deltaX * deltaX ) + (deltaY * deltaY ) );
+            // Current location
+            var x = move[0];
+            var y = move[1];
 
- 			var s = h + g;
- 			return -s;
- 		}
- 		
- 		// Naive implementation of a priority queue
- 		function getBest(moves){
- 			var bestScore = -1000;
- 			var bestIndex = 0;
+            // cost to get here
+            g = depth[makeKey(move)];
 
- 			for (var i = 0;i<moves.length;i++){
+            // Manhattan heuristic
+            deltaX = Math.abs(x - goalX);
+            deltaY = Math.abs(y - goalY);
 
- 				if (score[makeKey(moves[i])] > bestScore){
- 					bestIndex = i;
- 					bestScore = score[makeKey(moves[i])];
- 				}
- 			}
- 			return bestIndex;
- 		}
+            var h =  Math.sqrt(( deltaX * deltaX ) + (deltaY * deltaY ) );
 
- 		// instatiate data structures
- 		var open = [[]];
- 		var closed = [[]];
- 		var score = {};
- 		var depth = {};
+            // Combine known cost with estimate 
+            var score = h + g;
+            return -score;
+        }
+        
+        // Naive implementation of a priority queue
+        function getBest(moves){
+            var bestScore = -1000;
+            var bestIndex = 0;
 
- 		// initialize data structures
- 		open[0]=position;
- 		closed[0]=position;
- 		depth[makeKey(position)] = 0;
+            for (var i = 0;i<moves.length;i++){
 
- 		while (open.length > 0){
- 			this.count++;
- 			
- 			var bestIndex = getBest(open);
- 			var currentPosition = open[bestIndex];
+                if (score[makeKey(moves[i])] > bestScore){
+                    bestIndex = i;
+                    bestScore = score[makeKey(moves[i])];
+                }
+            }
+            return bestIndex;
+        }
 
- 			open.splice(bestIndex);
+        // instatiate data structures
+        var open = [[]];
+        var closed = [[]];
+        var score = {};
+        var depth = {};
 
- 			if (!contains(closed,currentPosition)){
- 				closed.push(currentPosition);
+        // initialize data structures
 
- 			}
-			var currentScore = calculateScore(currentPosition,goal);
+        // Open holds nodes to be evaluated
+        open[0]=position;
 
-			if(score[makeKey(currentPosition)]){
-				if(currentScore < score[makeKey(currentPosition)]){
-					score[makeKey(currentPosition)] = currentScore;
-				}
-			}else{
-				score[makeKey(currentPosition)] = currentScore;
-			}
-			
- 			if (currentPosition[0] == goal[0] && currentPosition[1] == goal[1]){
- 				return this.pathFrom(goal);
- 			}
- 	
- 			if (this.debug){
- 				print ("");
- 				matrix.show();
- 			}
- 			var moves = matrix.possibleMoves(currentPosition);
+        // Closed holds nodes that have been evaluated
+        closed[0]=position;
+        depth[makeKey(position)] = 0;
+
+        while (open.length > 0){
+            this.count++;
+
+            // Get first item in priority queue
+            var bestIndex = getBest(open);
+            var currentPosition = open[bestIndex];
+
+            // Remove selected item from priority queue
+            open.splice(bestIndex);
+
+            // Add to evaluated nodes
+            if (!contains(closed,currentPosition)){
+                closed.push(currentPosition);
+
+            }
+            var currentScore = calculateScore(currentPosition,goal);
+
+            // If this node has been scored, check if new score is better
+            if(score[makeKey(currentPosition)]){
+                if(currentScore > score[makeKey(currentPosition)]){
+                    score[makeKey(currentPosition)] = currentScore;
+                }
+            }else{
+            // Record score
+                score[makeKey(currentPosition)] = currentScore;
+            }
+            
+            // Goal reached
+            if (currentPosition[0] == goal[0] && currentPosition[1] == goal[1]){
+                return this.pathFrom(goal);
+            }
+    
+            if (this.debug){
+                print ("");
+                matrix.show();
+            }
+
+            // Get all possible moves for current node
+            var moves = matrix.possibleMoves(currentPosition);
+
+            for (var i = 0; i < moves.length; i++){
+
+                    this.count++;
+                    var d = depth[makeKey(currentPosition)];    
+
+                    x = moves[i][0];
+                    y = moves[i][1];
+
+                    // Increment length of path 
+                    depth[x+","+y] = d+1;
+
+                // Re-evaluate closed nodes if the new estimated value is better
+                if(contains(closed,moves[i])){
+                    var prior = score[makeKey(moves[i])];
+                    var currentScore = calculateScore(moves[i],goal);
+                    if  (currentScore > prior){
+                        score[makeKey(moves[i])] = currentScore;
+                        closed.splice(closed.indexOf(moves[i]));
+                        if (!contains(open,moves[i])){
+                            this.predecessor[makeKey(moves[i])] = currentPosition;
+                            open.push(moves[i]);
+                        };
+                    }
+                }else{
+
+                    // Add previously unevaluated node to queue
+                    if (!contains(open,moves[i])){
+                        this.predecessor[makeKey(moves[i])] = currentPosition;
+                        score[makeKey(moves[i])] = calculateScore(moves[i],goal);
+
+                        open.push(moves[i]);
+                    }
+                }
+
+            }
+        }
 
 
- 			for (var i = 0; i < moves.length; i++){
 
- 					this.count++;
- 					var d = depth[makeKey(currentPosition)];	
+        return([]);
+    }
 
- 					x = moves[i][0];
- 					y = moves[i][1];
- 					depth[x+","+y] = d+1;
-
- 				if(contains(closed,moves[i])){
- 					var prior = score[makeKey(moves[i])];
-					var currentScore = calculateScore(moves[i],goal);
- 					if  (currentScore > prior){
- 						score[makeKey(moves[i])] = currentScore;
- 						closed.splice(closed.indexOf(moves[i]));
- 						if (!contains(open,moves[i])){
-							this.predecessor[makeKey(moves[i])] = currentPosition;
-							open.push(moves[i]);
-						};
- 					}
- 				}else{
-
- 					if (!contains(open,moves[i])){
-						this.predecessor[makeKey(moves[i])] = currentPosition;
-						score[makeKey(moves[i])] = calculateScore(moves[i],goal);
-
-						open.push(moves[i]);
-					}
-				}
-
-			}
- 		}
+    // Walk back along breadcrumb path from goal node 
+    Search.prototype.pathFrom = function(position){
 
 
+        flag = 1;
+        var moves = [];
+        var x = position[0];
+        var y = position[1];
+        while (flag){
+            if (this.predecessor[x+","+y] ){
+                moves.unshift(this.predecessor[x+","+y] );
+                var newX = this.predecessor[x+","+y][0];
+                var newY = this.predecessor[x+","+y][1];
 
- 		return([]);
-	}
+                x = newX;
+                y = newY;
+            }else{
+                flag = 0;
+            }
 
-	this.pathFrom = function(position){
+        }
 
-
-		flag = 1;
-		var moves = [];
-		var x = position[0];
-		var y = position[1];
-		while (flag){
-			if (this.predecessor[x+","+y] ){
-				moves.unshift(this.predecessor[x+","+y] );
-				var newX = this.predecessor[x+","+y][0];
-				var newY = this.predecessor[x+","+y][1];
-
-				x = newX;
-				y = newY;
-			}else{
-				flag = 0;
-			}
-
-		}
-
-		return moves;
-	}
+        return moves;
+    }
 }
 
 // Demo 
 
 
 var s = new Search();
-s.debug = 1;
+s.debug = 0;
 var g = new Grid();
 g.show();
-print ("Depth-First Search: ");
+print ("Depth-First Search: [6,7],[13,13]");
 showMoves (s.depthFirstSearch(g,[6,7],[13,13]));
 print (s.count + " steps");
 
@@ -372,8 +413,8 @@ print (s.count + " steps");
 s = new Search();
 s.debug = 0;
 g = new Grid();
-g.show();
-print ("Breadth-First Search: ");
+//g.show();
+print ("Breadth-First Search: [6,7],[13,13]");
 showMoves (s.breadthFirstSearch(g,[6,7],[13,13]));
 print (s.count + " steps");
 
@@ -381,7 +422,7 @@ print (s.count + " steps");
 s = new Search();
 s.debug = 0;
 g = new Grid();
-g.show();
-print ("A* Search: ");
+//g.show();
+print ("A* Search: [6,7],[13,13]");
 showMoves (s.aStar(g,[6,7],[13,13]));
 print (s.count + " steps");
